b82ab2ce5a894d5f04decfc2dff6635fcf0b19a8
msroczek <maciej.sroczek@gmail.com>
2022-05-09 16:39:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# City Guide App
+
+A simple City Guide Flutter App for Mobile And Multimedia Systems (PWr).
+
+- To run the app, execute: `flutter run`
---
+++ android/app/src/main/kotlin/pl/edu/pwr/city_guide/i242450/mams_city_guide/MainActivity.kt
@@ -0,0 +1,6 @@
+package pl.edu.pwr.city_guide.i242450.mams_city_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
